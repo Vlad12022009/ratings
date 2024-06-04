482b4dd1abe88ce650dffdb1d9d7e3a4d9446ff4
Vlad12022009 <vladikus1202@gmail.com>
2024-06-04 16:39:12 +0300

    page fixed

--- pagination_example/views.py
+++ pagination_example/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render
 from rating.models import Rating
-import math
 # Create your views here.
 
 def pagination_view(request, *args, **kwargs):
+    from math import ceil
     qs = Rating.objects.all()
     page = int(request.GET.get('page', '1'))
     paginate_by = 5
-    max_page = math.ceil(len(qs)/paginate_by)
+    max_page = ceil(len(qs)/paginate_by)
     index = (page-1)*paginate_by
-    #page = max_page if page > max_page else page
-    #page = page+1 if page < 1 else page
+    next_page = page+1
+    last_page = page-1
+
     if page < 1: 
         start = 0
         end = 5
+        next_page = 2
     elif page > max_page:
         start = (max_page*paginate_by)-5
         end = max_page*paginate_by
+        last_page = max_page - 1
     else:
         start = index
         end = index+5
+
     return render(request, 'pagination_example/pagination_example.html',
-                {'page': qs[start:end],
-                'next_page': page+1, 'last_page': page-1 })
+                {'page': qs[start:end], 'next_page': next_page, 'last_page': last_page })
